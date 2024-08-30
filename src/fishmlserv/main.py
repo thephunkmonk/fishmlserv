cd7346c6582bad83e68ee18e186b0aa805507a3b
thephunkmonk <andrewchoi1218@gmail.com>
2024-08-30 12:14:16 +0900

    added preds

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -11,3 +11,25 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
 	return {"item_id": item_id, "q": q}
 
+@app.get("/fish")
+def fish(length: float, weight:float):
+
+	"""
+	determining the kind of fish
+	
+	args:
+		length (float) : fish length in cm
+		weight (float): fish weight in g
+	
+	returns:
+		dict: dictionary of kindof fish
+	"""
+	pred = "idk"
+	if length > 30.0:
+		pred = "도미"
+	else:
+		pred = "빙어"
+	return {
+		"pred" : pred,
+		"length" : length,
+		"weight" : weight}
